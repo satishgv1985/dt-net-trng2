7cd3616ecd1cdfa2734a5ec4753de9fa11bdddf8
srinivasarao.snv <srinivasarao.snv@7566c598-9965-1030-85f5-2caf12b33ae7>
2012-03-07 00:49:48 +0000

    Updated table scripts

---
+++ AspNetTrainingClasses/TNSRTCdb/Schema Objects/Schemas/dbo/Tables/tblBooking.table.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[tblBooking]
+(
+	BookingId int NOT NULL Identity(1,1), 
+	UserID int NULL,
+	ServiceID int Null,
+	AmtPerTkt numeric(14,4) Null,
+	NoofTkts int Null,
+	TotalAmt numeric(14,4) Null,
+	PaymentType varchar(50) Null, -- Cash, Credit Card, Debit Card, Cheque, Net Banking etc.
+	ChequeNo varchar(50) Null,
+	Chequedate DateTime Null,
+	BankName varchar(50) Null,
+	DateOfBooking DateTime Null,
+	PlaceofBooking varchar(50) Null,
+	BoardingPoint varchar(50) Null,
+	ReservationUpto varchar(50) Null,
+	Passenger1Name varchar(50) Null,
+	Passenger1ContactNo varchar(10) Null,
+)
---
+++ AspNetTrainingClasses/TNSRTCdb/Schema Objects/Schemas/dbo/Tables/tblState.table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tblState]
+(
+	StateID int NOT NULL Identity(1,1), 
+	StateName varchar(50) NULL,
+	CountryName varchar(50)
+)
