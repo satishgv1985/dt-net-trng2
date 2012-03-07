21a8f96538cc5c4690c909d135118d127b9c86cb
srinivasarao.snv <srinivasarao.snv@7566c598-9965-1030-85f5-2caf12b33ae7>
2012-03-07 00:53:55 +0000

    Modified and moved to Tables folder

--- trunk/AspNetTrainingClasses/TNSRTCdb/tblBus.table.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[tblBus]
-(
-	Bus_Id int NOT NULL Identity(1,1), 
-	Bus_Type_Id int NULL
-
-)
--- trunk/AspNetTrainingClasses/TNSRTCdb/tblService.table.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[tblService]
-(
-	Service_Id int Not Null Identity(1,1),
-	Service_No int Null,
-	From_Place_ID int Null,
-	To_Place_ID int Null
-)
