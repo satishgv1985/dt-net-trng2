c226b435da421611db7a0a564397693105dbe468
anupam.east@gmail.com <anupam.east@gmail.com@7566c598-9965-1030-85f5-2caf12b33ae7>
2012-03-07 18:09:33 +0000

    Booking.cs file is added

---
+++ AspNetTrainingClasses/TNSRTC/Booking.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TNSRTC
+{
+    class Booking
+    {
+       public int BookingId {get; set;}
+       public int UserID  {get; set;}
+       public int ServiceID  {get; set;}
+       public int AmtPerTkt  {get; set;}
+       public int NoofTkts  {get; set;}
+       public int TotalAmt  {get; set;}
+       public bool PaymentType  {get; set;}
+       public int ChequeNo  {get; set;}
+       public DateTime Chequedate  {get; set;}
+       public string BankName  {get; set;}
+       public DateTime DateOfBooking  {get; set;}
+       public string PlaceofBooking  {get; set;}
+       public string BoardingPoint  {get; set;}
+       public string ReservationUpto  {get; set;}
+       public string Passenger1Name  {get; set;}
+       public int Passenger1ContactNo { get; set; }
+    }
+}
