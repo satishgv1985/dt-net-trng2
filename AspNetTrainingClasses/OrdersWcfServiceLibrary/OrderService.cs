a63e721e780dce56bb9d1ff8a38f317918f49ddc
satishgv1985 <satishgv1985@7566c598-9965-1030-85f5-2caf12b33ae7>
2012-03-14 18:37:51 +0000

    Added WCF Hosting in IIS and in Windows Service.

---
+++ AspNetTrainingClasses/OrdersWcfServiceLibrary/OrderService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace OrdersWcfServiceLibrary
+{
+    [DataContract]
+    public class Order
+    {
+        [DataMember]
+        public int OrderId;
+        [DataMember]
+        public string OrderAddress;
+        [DataMember]
+        public DateTime OrderDate;
+    }
+
+    [ServiceContract]
+    public interface IOrderService
+    {
+        [OperationContract]
+         Order GetOrderById(int id);
+        [OperationContract]
+         void PlaceOrder(Order o);
+    }
+
+    public class OrderService :IOrderService
+    {
+        List<Order> listOrders = new List<Order>();
+        public Order GetOrderById(int id)
+        {
+            return listOrders.Find(x => x.OrderId == id);
+        }
+
+        public void PlaceOrder(Order o)
+        {
+            listOrders.Add(o);
+        }
+    }
+}
---
+++ AspNetTrainingClasses/OrdersWindowsService/OrderService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.ServiceModel;
+
+namespace OrdersWindowsService
+{
+    public partial class OrderService : ServiceBase
+    {
+        ServiceHost svc = new ServiceHost(typeof(OrdersWcfServiceLibrary.OrderService));
+       
+        public OrderService()
+        {
+            InitializeComponent();
+           
+            
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            svc.Open();
+        }
+
+        protected override void OnStop()
+        {
+            svc.Close();
+        }
+    }
+}
